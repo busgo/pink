c5c81d2c9222883e89e534cf116fd23ea556aaf4
busgo <248434199@qq.com>
2021-09-09 18:56:02 +0800

    pink collector

--- collector/collector.go
+++ collector/collector.go
@@ -2,27 +2,31 @@ package collector
 
 import (
 	"context"
+	"github.com/busgo/pink/db/repository"
 	"github.com/busgo/pink/pkg/bus"
 	"github.com/busgo/pink/pkg/etcd"
+	"github.com/busgo/pink/pkg/log"
 	"github.com/busgo/pink/pkg/protocol"
-	"log"
 	"strings"
+	"time"
 )
 
 // PinkCollector  collect the execute snapshot
 type PinkCollector struct {
-	cli      *etcd.Cli
-	eventBus *bus.EventBus
-	state    int
+	cli                       *etcd.Cli
+	eventBus                  *bus.EventBus
+	state                     int
+	executeSnapshotRepository *repository.ExecuteSnapshotRepository
 }
 
 // new PinkCollector
-func NewPinkCollector(cli *etcd.Cli, eventBus *bus.EventBus) *PinkCollector {
+func NewPinkCollector(executeSnapshotRepository *repository.ExecuteSnapshotRepository, cli *etcd.Cli, eventBus *bus.EventBus) *PinkCollector {
 
 	c := &PinkCollector{
-		cli:      cli,
-		eventBus: eventBus,
-		state:    protocol.Follower,
+		cli:                       cli,
+		eventBus:                  eventBus,
+		state:                     protocol.Follower,
+		executeSnapshotRepository: executeSnapshotRepository,
 	}
 
 	go c.lookup()
@@ -46,12 +50,13 @@ func (c *PinkCollector) lookup() {
 // handle execute snapshot change
 func (c *PinkCollector) handleExecuteSnapshotChange(kc *etcd.KeyChange) {
 	if c.state != protocol.Leader {
+		log.Infof("the pink collector ")
 		return
 	}
 
 	switch kc.Event {
 	case etcd.KeyUpdateChangeEvent:
-		log.Printf("the pink collector receive the execute history snapshot update event %+v", kc)
+		log.Debugf("the pink collector receive the execute history snapshot update event %+v", kc)
 		c.dealExecuteSnapshotChange(kc)
 	}
 }
@@ -62,17 +67,17 @@ func (c *PinkCollector) dealExecuteSnapshotChange(kc *etcd.KeyChange) {
 		_ = c.cli.Delete(context.Background(), kc.Key)
 		return
 	}
+	es := new(protocol.ExecuteSnapshot).Decode(kc.Value)
+	if es.State == protocol.ExecuteSnapshotFail || es.State == protocol.ExecuteSnapshotSuccess {
 
-	// TODO  transfer to MySQL or  Mongo db ES
-	//es := new(protocol.ExecuteSnapshot).Decode(kc.Value)
-	//if es.State == protocol.ExecuteSnapshotFail || es.State == protocol.ExecuteSnapshotSuccess {
-	//	log.Printf("the pink collector start  transfer the execute snapshot %+v", kc)
-	//	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-	//	err := c.cli.Transfer(ctx, kc.Key, fmt.Sprintf("%s%s", protocol.ExecuteSnapshotHistoryPath, es.Id), kc.Value)
-	//	if err != nil {
-	//		log.Printf("the pink collector transfer the execute snapshot %+v fail", kc)
-	//		return
-	//	}
-	//	log.Printf("the pink collector transfer the execute snapshot %+v success", kc)
-	//}
+		c.executeSnapshotRepository.Insert(nil)
+		log.Infof("the pink collector start  transfer the execute snapshot %+v", kc)
+		ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
+		err := c.cli.Delete(ctx, kc.Key)
+		if err != nil {
+			log.Infof("the pink collector transfer the execute snapshot %+v fail", kc)
+			return
+		}
+		log.Infof("the pink collector transfer the execute snapshot %+v success", kc)
+	}
 }
--- db/repository/execute_snapshot_repository.go
+++ db/repository/execute_snapshot_repository.go
@@ -2,7 +2,12 @@ package repository
 
 import (
 	"github.com/busgo/pink/db"
+	"github.com/busgo/pink/db/model"
 	"github.com/jmoiron/sqlx"
+)
+
+const (
+	InsertExecuteSnapshotSql = "INSERT INTO execute_snapshot (`snapshot_id`,`job_name`,`group`,`cron`,`target`,`ip`,`param`,`state`,`before_time`,`schedule_time`,`end_time`,`times`,`mobile`,`remark`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
 )
 
 // ExecuteSnapshotRepository
@@ -13,4 +18,9 @@ type ExecuteSnapshotRepository struct {
 // new ExecuteSnapshotRepository
 func NewExecuteSnapshotRepository(db *db.DB) *ExecuteSnapshotRepository {
 	return &ExecuteSnapshotRepository{connection: db.DS}
+}
+
+func (repo *ExecuteSnapshotRepository) Insert(snapshot *model.ExecuteSnapshot) {
+
+	repo.connection.Exec(InsertExecuteSnapshotSql, snapshot)
 }
